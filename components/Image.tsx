220ade919498a9de6f12d6731af453db9d6a219a
ost-son.nguyen <son.nguyen@ostechnology.co.jp>
2024-04-12 18:09:40 +0900

    add personal information(個人情報を追加)

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,7 +1,5 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`/samyoney.github.io${src}`} {...rest} />
-)
+const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
 
 export default Image
