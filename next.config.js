ed73ded32d91e9fa5a440812f7f30da7caf256ac
ost-son.nguyen <son.nguyen@ostechnology.co.jp>
2024-04-12 11:50:19 +0900

    fixbug

--- next.config.js
+++ next.config.js
@@ -65,7 +65,7 @@ module.exports = () => {
     eslint: {
       dirs: ['app', 'components', 'layouts', 'scripts'],
     },
-    basePath: '/samyoney.github.io',
+    // basePath: '/samyoney.github.io',
     output: 'export',
     images: {
       unoptimized: true,
